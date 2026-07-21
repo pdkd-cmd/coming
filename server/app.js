036f6b0be1c7340ec96f631e50921afc37b2b298
Sanjeev Narayan <khawaresanjeevpdkd@gmail.com>
2026-07-21 21:43:13 +0530

    Serve frontend and admin from Express

--- server/app.js
+++ server/app.js
@@ -35,8 +35,17 @@ app.use("/api/banners", bannerRoutes);
 app.use("/api/settings", settingsRoutes);
 app.use("/api/product-images", verifyToken, productImageRoutes);
 
+const path = require("path");
+
+// Serve website files
+app.use(express.static(path.join(__dirname, "../website")));
+
+// Serve admin panel
+app.use("/admin", express.static(path.join(__dirname, "../admin")));
+
+// Homepage
 app.get("/", (req, res) => {
-  res.send("PDKD Backend Running 🚀");
+  res.sendFile(path.join(__dirname, "../website/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
